2f56c08641580cafb010e311d9b9c986fab97fcb
Trung <TrungNV984@gmail.com>
2020-12-29 15:03:06 +0700

    experiment

--- server.php
+++ server.php
@@ -1,8 +1,8 @@
 <?php
-$http = new Swoole\HTTP\Server("0.0.0.0", 9501);
+$http = new Swoole\HTTP\Server("0.0.0.0", 38030);
 
 $http->on('start', function ($server) {
-    echo "Swoole http server is started at http://127.0.0.1:9501\n";
+    echo "Swoole http server is started at http://127.0.0.1:38030\n";
 });
 
 $http->on('request', function ($request, $response) {
