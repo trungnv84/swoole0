02f521da1af980d7b8f2a5ef16cdf568b0b77c1a
Trung <TrungNV984@gmail.com>
2020-12-31 13:03:55 +0700

    test

--- test.php
+++ test.php
@@ -4,10 +4,12 @@ require 'lib/ravendb/session.php';
 
 $raven = new \RavenDB\Session('http://192.168.0.102:28080', 'omgfin-exchange');
 
-var_dump($raven->add('product/', [
-    'name' => 'Window 10',
-    'price' => '300',
-    '@metadata' => [
-        '@collection' => 'Products'
-    ]
-]));
+Co\run(function () use ($raven) {
+    var_dump($raven->add('product/', [
+        'name' => 'Window 10',
+        'price' => '300',
+        '@metadata' => [
+            '@collection' => 'Products'
+        ]
+    ]));
+});
