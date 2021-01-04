c873e93a69a3ce46f1e9aaea25ba0a068e441e1f
Trung <TrungNV984@gmail.com>
2021-01-04 11:29:33 +0700

    test

--- test.php
+++ test.php
@@ -9,7 +9,7 @@ go(function () {
     $redis = new \EasySwoole\Redis\Redis(new \EasySwoole\Redis\Config\RedisConfig([
         'host' => '127.0.0.1',
         'port' => '6379',
-        'auth' => 'easyswoole',
+        'auth' => '',
         'serialize' => \EasySwoole\Redis\Config\RedisConfig::SERIALIZE_NONE
     ]));
     var_dump($redis->set('a',1));
