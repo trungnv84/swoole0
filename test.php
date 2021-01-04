d7e7ec99ea95ebec71f23ddc9fa4607c4e7cacff
Trung <TrungNV984@gmail.com>
2021-01-04 10:20:15 +0700

    test

--- test.php
+++ test.php
@@ -1,10 +1,9 @@
 <?php
-use EasySwoole\FastCache\Cache;
-use EasySwoole\FastCache\Job;
-
 require 'vendor/autoload.php';
 require 'lib/raven/session.php';
 
+use EasySwoole\FastCache\Cache;
+use EasySwoole\FastCache\Job;
 
 $job = new Job();
 $job->setData("siam"); // any type of data
